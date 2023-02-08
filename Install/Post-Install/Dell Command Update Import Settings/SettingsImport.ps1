d12933cc47db48d97e56619d54e69e898d08ff0d
vartaxe <vartaxe@hotmail.com>
2023-02-08 14:06:55 +0100

    Update SettingsImport.ps1

--- Install/Post-Install/Dell Command Update Import Settings/SettingsImport.ps1
+++ Install/Post-Install/Dell Command Update Import Settings/SettingsImport.ps1
@@ -1,4 +1,4 @@
-$xmlName = "CHANGEME.xml"
-Push-Location "${env:ProgramFiles(x86)}\Dell\CommandUpdate\" 
+$xmlName = "MySettings.xml"
+Push-Location "${env:ProgramFiles}\Dell\CommandUpdate\" 
 $rc = Start-Process "dcu-cli.exe" -wait -ArgumentList "/configure","-importSettings=$PSScriptRoot\$xmlName"  -PassThru -ErrorAction "Stop" 
 return $rc.ExitCode
